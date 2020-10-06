efd22aae16131cd5982280a799f0535da2e774af
Gabriel Bicudo Rosa <gabrielbicudorosa@gmail.com>
2020-10-06 20:21:29 -0300

    atualizando codigo

--- scrap.py
+++ scrap.py
@@ -1,7 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
+import mysql.connector 
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver 
 
-site = requests.get('https://baixarmusica.me').text
+sql_user = {
+            'user':'Gabriel', 
+            'password':'Tg@briel10',
+            'host':'127.0.0.1',
+            'port':'3306',
+            'database':'web'}
+
+cnx =  mysql.connector.connect(**sql_user)
+
+cursor = cnx.cursor()
+
+options = webdriver.chrome.options()
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.get('https://baixarmusica.me')
+
+last_pag = driver.find_elements_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div/a[4]').click()
+
+print(last_pag)
+
 
 stxt = BeautifulSoup(site,'lxml')
 
@@ -15,4 +36,11 @@ for articles in stxt.find_all('article'):
 
     download = articles.find('footer',class_='entry-footer cf').a['href']
 
+    cursor.execute("insert into webscraping(url,url_down,tittle,summary) values ('{0}','{1}','{2}','{3}');".format(url,download,titles,summary))
+
     print(titles,'\n',url,'\n',summary,'\n',download,'\n\n')
+
+    cnx.commit()
+
+cursor.close()
+cnx.close()
